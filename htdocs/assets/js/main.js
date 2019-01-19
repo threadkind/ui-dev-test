75929c9b044c8d15544781fc826df7fb90f3555f
threadkind <threadkind@gmail.com>
2019-01-18 19:41:13 -0600

    feat: Add hamburger menu

--- htdocs/assets/js/main.js
+++ htdocs/assets/js/main.js
@@ -1,1 +1,12 @@
 // Custom javascript
+
+const menuOptions = document.querySelectorAll('.menu-option');
+
+document.getElementById('menu-button').addEventListener('click', function(){
+
+	for(let i = 0; i < menuOptions.length; i++) {
+
+		menuOptions[i].classList.toggle('isOpen');
+	}
+
+});
