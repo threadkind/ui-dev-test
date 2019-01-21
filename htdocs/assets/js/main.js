5374e5f76a86a10c057d4c6ed179952187950177
threadkind <threadkind@gmail.com>
2019-01-20 22:36:41 -0600

    feat: Add option to open menu using enter key

--- htdocs/assets/js/main.js
+++ htdocs/assets/js/main.js
@@ -2,18 +2,7 @@
 
 const menuOptions = document.querySelectorAll('.menu-option');
 
-const navbarMenu = document.querySelector('.navbar-menu');
-
-
-document.getElementById('menu-button').addEventListener('click', function(){
-
-	// if(navbarMenu.classList.contains('isHidden')){
-	// 	navbarMenu.classList.remove('isHidden');
-	// }
-	// else{
-	// 	setTimeout(navbarMenu.classList.add('isHidden'), 500);
-	// };
-
+function openMenu() {
 	for(let i = 0; i < menuOptions.length; i++) {
 
 		menuOptions[i].classList.toggle('isOpen');
@@ -22,5 +11,15 @@ document.getElementById('menu-button').addEventListener('click', function(){
 	document.querySelector('.trigram').classList.toggle('isHidden');
 
 	document.querySelector('.x').classList.toggle('isHidden');
+
+}
+
+document.getElementById('menu-button').addEventListener('click', openMenu);
+
+document.getElementById('menu-button').addEventListener('keypress', function(e){
+
+	if(e.keyCode === 13){
+		openMenu();
+	}
 
 });
