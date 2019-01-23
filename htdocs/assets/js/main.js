b4c821f79c3672e557a382894eb9cd87d982de2c
threadkind <threadkind@gmail.com>
2019-01-22 20:27:42 -0600

    feat: Fix menu animation to include display none for a11y

--- htdocs/assets/js/main.js
+++ htdocs/assets/js/main.js
@@ -2,11 +2,32 @@
 
 const menuOptions = document.querySelectorAll('.menu-option');
 
-function openMenu() {
-	for(let i = 0; i < menuOptions.length; i++) {
+const navbarMenu = document.querySelector('.navbar-menu');
 
-		menuOptions[i].classList.toggle('isOpen');
+function menuItemsToggle(){
+  for(let i = 0; i < menuOptions.length; i++) {
+
+    menuOptions[i].classList.toggle('isOpen');
 	}
+}
+
+function toggleMenu() {
+
+  if(navbarMenu.classList.contains('isHidden')){
+
+	navbarMenu.classList.toggle('isHidden');
+
+	setTimeout(menuItemsToggle, 50);
+
+  }
+  else {
+  	menuItemsToggle();
+
+  	setTimeout(function(){
+      navbarMenu.classList.toggle('isHidden');
+  	}, 350);
+
+  }
 
 	document.querySelector('.trigram').classList.toggle('isHidden');
 
@@ -14,12 +35,12 @@ function openMenu() {
 
 }
 
-document.getElementById('menu-button').addEventListener('click', openMenu);
+document.getElementById('menu-button').addEventListener('click', toggleMenu);
 
 document.querySelector('.trigram').addEventListener('keypress', function(e){
 
 	if(e.keyCode === 13){
-		openMenu();
+		toggleMenu();
 	}
 
 });
